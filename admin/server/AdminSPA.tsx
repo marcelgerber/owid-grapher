55795ad2902349b45848bce9630acc2ac5a5d745
Marcel Gerber <mgerber59@gmail.com>
2020-06-26 21:31:46 +0200

    ...

--- admin/server/AdminSPA.tsx
+++ admin/server/AdminSPA.tsx
@@ -22,7 +22,7 @@ export function AdminSPA(props: { username: string; isSuperuser: boolean }) {
                 <title>owid-admin</title>
                 <meta name="description" content="" />
                 <link
-                    href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i|Playfair+Display:400,700&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400;1,700&family=Playfair+Display:wght@400;700&display=swap"
                     rel="stylesheet"
                 />
                 <link
--- site/server/views/Head.tsx
+++ site/server/views/Head.tsx
@@ -51,7 +51,7 @@ export const Head = (props: {
             <meta name="twitter:description" content={pageDesc} />
             <meta name="twitter:image" content={imageUrl} />
             <link
-                href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i|Playfair+Display:400,700&display=swap"
+                href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400;1,700&family=Playfair+Display:wght@400;700&display=swap"
                 rel="stylesheet"
             />
             <link rel="stylesheet" href={webpack("commons.css", "site")} />
